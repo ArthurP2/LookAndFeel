e397bbc8fd2984180308d2626b3f0e05906a9db5
ExGenere <panlilioarthur@yahoo.com>
2017-02-07 05:26:58 -0800

    Initial commit

---
+++ app/src/main/java/apanlili/uw/tacoma/edu/lookandfeel/AnimationsActivity.java
@@ -0,0 +1,50 @@
+package apanlili.uw.tacoma.edu.lookandfeel;
+
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AnimationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animations);
+    }
+
+    public void animateSpin(View view) {
+        View v = findViewById(R.id.animate_me);
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(this,
+                R.animator.rotate_and_spin);
+        set.setTarget(v);
+        set.start();
+    }
+
+    public void animateMove(View view) {
+        View v = findViewById(R.id.animate_me);
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(this,
+                R.animator.move);
+        set.setTarget(v);
+        set.start();
+    }
+
+    public void animateFade(View view) {
+        View v = findViewById(R.id.animate_me);
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(this,
+                R.animator.fade);
+        set.setTarget(v);
+        set.start();
+    }
+
+    public void animateWhat(View view) {
+        View v = findViewById(R.id.animate_me);
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(this,
+                R.animator.what);
+        set.setTarget(v);
+        set.start();
+    }
+
+
+}
